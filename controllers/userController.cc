f97ea000a181d6f88b7dd41361d0e2ab9c287aa1
MahdiKarami <67632452+mahdikarami8484@users.noreply.github.com>
2025-08-27 16:34:42 +0000

    use response message

--- controllers/userController.cc
+++ controllers/userController.cc
@@ -1,17 +1,20 @@
 #include "userController.h"
 #include <json/json.h>  // Json::Value
+#include "responses/userResponse.hpp"
 
 using namespace drogon;
 
 // Add definition of your processing function here
 
-void UserController::createUser(const drogon::HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback)
+void UserController::registerUser(const drogon::HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback)
 {
-    Json::Value json;
-    json["message"] = "Hello World!";
-    json["success"] = true;
-    json["count"] = 123;
+    UserResponse response;
+    response.type = UserResponseType::REGISTER;
+    response.status = "success";
+    response.username = "exampleUser";
+    response.age = 25;
+    response.data["message"] = "User registered successfully";
 
-    auto resp = drogon::HttpResponse::newHttpJsonResponse(json); // این خودش Content-Type: application/json می‌ذاره
+    auto resp = drogon::HttpResponse::newHttpJsonResponse(response.toJson()); // این خودش Content-Type: application/json می‌ذاره
     callback(resp);
 }
--- controllers/userController.h
+++ controllers/userController.h
@@ -10,9 +10,12 @@ class UserController : public drogon::HttpController<UserController>
   public:
     METHOD_LIST_BEGIN
 
-    ADD_METHOD_TO(UserController::createUser, "/users/add", Get);
+    ADD_METHOD_TO(UserController::registerUser, "api/user/register", Post);
     
     METHOD_LIST_END
 
-    void createUser(const drogon::HttpRequestPtr &req, std::function<void (const HttpResponsePtr &)> &&callback);
+    void registerUser(
+      const drogon::HttpRequestPtr &req, 
+      std::function<void (const HttpResponsePtr &)> &&callback
+    );
 };
